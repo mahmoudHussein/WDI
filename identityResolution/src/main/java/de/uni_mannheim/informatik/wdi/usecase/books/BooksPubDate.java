38f8883a13a38df482aa16a91ee27c8f291dff27
mahmoudHussein <m.hussein0793@gmail.com>
2015-11-14 13:13:52 +0100

    added the books publication date class

---
+++ identityResolution/src/main/java/de/uni_mannheim/informatik/wdi/usecase/books/BooksPubDate.java
@@ -0,0 +1,73 @@
+package de.uni_mannheim.informatik.wdi.usecase.books;
+
+import de.uni_mannheim.informatik.wdi.Record;
+
+public class BooksPubDate extends Record{
+
+	/*
+	 * <Book>
+		<Id>DbpediaBooksPubDate_1</Id>
+		<ISBN>0</ISBN>
+		<Book_Name>Running Dog (novel)</Book_Name>
+		<Author>
+			<Name>Don DeLillo</Name>
+		</Author>
+		<Publisher>Alfred A. Knopf</Publisher>
+		<Publication_Date>1978</Publication_Date>
+	</Book>
+	
+	 */
+	
+	private String ISBN;
+	private String book_name;
+	private String author;
+	private String publisher;
+	private String year;
+	
+	public BooksPubDate(String identifier, String provenance) {
+		super(identifier, provenance);
+
+	}
+	
+	public String getISBN(){
+		return this.ISBN;
+	}
+	
+	public void setISBN(String isbn){
+		this.ISBN = isbn;
+	}
+	
+	public String getBookName(){
+		return this.book_name;
+	}
+	
+	public void setBookName(String bookName){
+		this.book_name = bookName;
+	}
+	
+	public String getAuthor(){
+		return this.author;
+	}
+	
+	public void setAuthor(String author){
+		this.author = author;
+	}
+	
+	public String getPublisher(){
+		return this.publisher;
+	}
+	
+	public void setPublisher(String publisher){
+		this.publisher = publisher;
+	}
+	
+	public String getYear(){
+		return this.year;
+	}
+	
+	public void setYear(String year){
+		this.year = year;
+	}
+	
+
+}

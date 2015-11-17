848d777d7db26573af622adff6ae71577be449aa
Aya El-Mobayad <aya.elmobayad@gmail.com>
2015-11-17 19:33:46 +0100

    added the ISBN comparator and te gitignore file

---
+++ identityResolution/src/main/java/de/uni_mannheim/informatik/wdi/usecase/books/comparators/BooksISBNComparator.java
@@ -0,0 +1,24 @@
+package de.uni_mannheim.informatik.wdi.usecase.books.comparators;
+
+import de.uni_mannheim.informatik.wdi.identityresolution.similarity.EqualsSimilarity;
+import de.uni_mannheim.informatik.wdi.identityresolution.matching.Comparator;
+import de.uni_mannheim.informatik.wdi.usecase.books.Books;
+
+public class BooksISBNComparator extends Comparator<Books> {
+
+	private EqualsSimilarity<String> sim = new EqualsSimilarity<String>();
+
+	public double compare(Books entity1, Books entity2) {
+
+		// Pre-processing
+		String ISBN1 = entity1.getISBN();
+		String ISBN2 = entity2.getISBN();
+
+		//Calculates Similarity between two ISBNs.
+		//Since ISBNs are considered universal identifiers for books,  
+		//they will matched for exact similarity
+		double similarity = sim.calculate(ISBN1, ISBN2);
+
+		return similarity;
+	}
+}
